123cc80a94b1df6159148c0fe1dc5a78dd0bc5aa
csg <imgokulcs@gmail.com>
2025-10-28 16:49:35 +0000

    Add priority field to services.Service model

--- apps/services/models.py
+++ apps/services/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Service(models.Model):
+    class Priority(models.TextChoices):
+        LOW = "LOW", "Low"
+        MEDIUM = "MEDIUM", "Medium"
+        HIGH = "HIGH", "High"
+
     name = models.CharField(max_length=200)
     description = models.TextField()
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices,
+        null = True
+    )
     base_price = models.DecimalField(max_digits=10, decimal_places=2)
     duration_hours = models.IntegerField()
     is_active = models.BooleanField(default=True)
